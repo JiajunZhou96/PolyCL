67242b740c0daa316789871115ebd1d036403c3b
Jiajun Zhou <91960648+JiajunZhou96@users.noreply.github.com>
2023-10-18 22:33:50 +0100

    Update train.py

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ config = utils.get_config(print_dict = False)
 seed = config["seed"]
 utils.set_seed(seed)
 
-pretrain_data = utils.read_txt(config["pretrain_data_txt"])
+pretrain_data = utils.read_txt(config["pretrain_data"])
 psmile_data = [dataloader.to_psmiles(smiles) for smiles in pretrain_data]
 pretrain_data = psmile_data
 
